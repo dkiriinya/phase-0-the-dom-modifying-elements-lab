7143ec21313aa76ac2bbff34ca33f6f3a3609fb5
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-07 23:28:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.getElementById("main")
+main.remove(main)
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+let yourname = 'Don'
+newHeader.textContent = `${yourname} is the champion`
+console.log(newHeader)
